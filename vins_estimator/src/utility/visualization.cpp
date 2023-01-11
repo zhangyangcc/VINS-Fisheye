7f3b2a80dea4bfd7a98079e993c3fd2c804b8a51
zhangyang <958827244@qq.com>
2023-01-11 12:18:43 +0800

    set extrinsic parameter in code

--- vins_estimator/src/utility/visualization.cpp
+++ vins_estimator/src/utility/visualization.cpp
@@ -178,15 +178,15 @@ void pubOdometry(const Estimator &estimator, const std_msgs::Header &header)
         ofstream foutC(VINS_RESULT_PATH, ios::app);
         foutC.setf(ios::fixed, ios::floatfield);
         foutC.precision(0);
-        foutC << header.stamp.toSec() * 1e9 << ",";
+        foutC << header.stamp.toSec() * 1e9 << " ";
         foutC.precision(5);
-        foutC << estimator.Ps[WINDOW_SIZE].x() << ","
-              << estimator.Ps[WINDOW_SIZE].y() << ","
-              << estimator.Ps[WINDOW_SIZE].z() << ","
-              << tmp_Q.w() << ","
-              << tmp_Q.x() << ","
-              << tmp_Q.y() << ","
-              << tmp_Q.z() << "," << endl;
+        foutC << estimator.Ps[WINDOW_SIZE].x() << " "
+              << estimator.Ps[WINDOW_SIZE].y() << " "
+              << estimator.Ps[WINDOW_SIZE].z() << " "
+              << tmp_Q.x() << " "
+              << tmp_Q.y() << " "
+              << tmp_Q.z() << " " 
+              << tmp_Q.w() << endl;
               //<< estimator.Vs[WINDOW_SIZE].x() << ","
               //<< estimator.Vs[WINDOW_SIZE].y() << ","
               //<< estimator.Vs[WINDOW_SIZE].z() << "," << endl;
