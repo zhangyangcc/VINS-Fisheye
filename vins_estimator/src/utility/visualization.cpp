2368ef24cfd2d7464fb6c1c76b18b76a8d50bb73
zhangyang <958827244@qq.com>
2023-02-06 15:55:58 +0800

    modify precision to adapt to tum format

--- vins_estimator/src/utility/visualization.cpp
+++ vins_estimator/src/utility/visualization.cpp
@@ -177,8 +177,8 @@ void pubOdometry(const Estimator &estimator, const std_msgs::Header &header)
         // write result to file
         ofstream foutC(VINS_RESULT_PATH, ios::app);
         foutC.setf(ios::fixed, ios::floatfield);
-        foutC.precision(0);
-        foutC << header.stamp.toSec() * 1e9 << " ";
+        foutC.precision(9);
+        foutC << header.stamp.toSec() << " ";
         foutC.precision(5);
         foutC << estimator.Ps[WINDOW_SIZE].x() << " "
               << estimator.Ps[WINDOW_SIZE].y() << " "
